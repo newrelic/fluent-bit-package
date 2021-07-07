754a018b7b83a0f837793d998da7905c0a9e7283
noly <noeliaddf@gmail.com>
2021-07-07 11:04:41 +0200

    feat: add build step

--- scripts/sign.sh
+++ scripts/sign.sh
@@ -3,38 +3,37 @@ set -e
 #
 #
 #
-# Sign/Resign RPM's & DEB's in /dist artifacts to GH Release Assets
+# Sign RPM's & DEB's in /packages to GH Release Assets
 #
 #
 #
 
 # Sign RPM's
-echo "===> Create .rpmmacros to sign rpm's from Goreleaser"
+echo "===> Create .rpmmacros to sign rpm"
 echo "%_gpg_name ${GPG_MAIL}" >> ~/.rpmmacros
 echo "%_signature gpg" >> ~/.rpmmacros
-echo "%_gpg_path /root/.gnupg" >> ~/.rpmmacros
+echo "%_gpg_path /home/runner/.gnupg" >> ~/.rpmmacros
 echo "%_gpgbin /usr/bin/gpg" >> ~/.rpmmacros
 echo "%__gpg_sign_cmd   %{__gpg} gpg --no-verbose --no-armor --batch --pinentry-mode loopback --passphrase ${GPG_PASSPHRASE} --no-secmem-warning -u "%{_gpg_name}" -sbo %{__signature_filename} %{__plaintext_filename}" >> ~/.rpmmacros
 
 echo "===> Importing GPG private key from GHA secrets..."
 printf %s ${GPG_PRIVATE_KEY_BASE64} | base64 -d | gpg --batch --import -
 
-echo "===> Importing GPG signature, needed from Goreleaser to verify signature"
+echo "===> Importing GPG signature, needed to verify signature"
 gpg --export -a ${GPG_MAIL} > /tmp/RPM-GPG-KEY-${GPG_MAIL}
 rpm --import /tmp/RPM-GPG-KEY-${GPG_MAIL}
 
-echo "===> List keys"
-rpm -q gpg-pubkey --qf '%{NAME}-%{VERSION}-%{RELEASE}\t%{SUMMARY}\n'
+cd packages
 
 for rpm_file in $(find -regex ".*\.\(rpm\)");do
   echo "===> Signing $rpm_file"
-  rpm --resign $rpm_file
+  rpm --addsign $rpm_file
   echo "===> Sign verification $rpm_file"
   rpm -v --checksig $rpm_file
 done
 
 # Sign DEB's
-GNUPGHOME="/root/.gnupg"
+GNUPGHOME="/home/runner/.gnupg"
 echo "${GPG_PASSPHRASE}" > "${GNUPGHOME}/gpg-passphrase"
 echo "passphrase-file ${GNUPGHOME}/gpg-passphrase" >> "$GNUPGHOME/gpg.conf"
 echo 'allow-loopback-pinentry' >> "${GNUPGHOME}/gpg-agent.conf"
