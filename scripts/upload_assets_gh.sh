3ca52f9b42a73b3e66ea2d6ef24c4ff1f2b8785b
noly <noeliaddf@gmail.com>
2021-07-07 10:31:31 +0200

    feat: include upload assests script

---
+++ scripts/upload_assets_gh.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -e
+#
+#
+# Upload artifacts to GH Release assets
+#
+#
+cd packages
+for filename in $(find  -regex ".*\.\(rpm\|deb\|zip\)");do
+  echo "===> Uploading to GH $VERSION: ${filename}"
+      gh release upload $VERSION $filename
+done
