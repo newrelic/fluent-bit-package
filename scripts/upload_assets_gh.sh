e013e5a585043ef746c78448772e6b1a8833f1f1
Daniel Rodríguez <daniel@dpstudios.es>
2023-02-02 12:23:51 +0100

    ci: fix upload step (#47)

--- scripts/upload_assets_gh.sh
+++ scripts/upload_assets_gh.sh
@@ -5,8 +5,8 @@ set -e
 # Upload artifacts to GH Release assets
 #
 #
-cd ~/packages
+cd packages
 for filename in $(find  -regex ".*\.\(rpm\|deb\|zip\)");do
   echo "===> Uploading to GH $VERSION: ${filename}"
-      gh release upload $VERSION $filename
+      gh release upload --clobber $VERSION $filename
 done
