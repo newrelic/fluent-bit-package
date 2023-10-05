5c39ae08fd7dbe433df8cf22c223690cd0ca4b06
Lucas <luckslovez@gmail.com>
2023-10-05 18:29:02 +0200

    Jest config

--- integration-tests/test-suite/setup-jest.js
+++
@@ -1,8 +0,0 @@
-const { WAIT_FOR_TEST_COMPLETION } = require('./lib/waitTimes');
-
-/**
- * Give Jest more time than the default 5 seconds. The Jest default is for
- * unit tests -- for E2E tests we want a far longer timeout.
- */
-jest.setTimeout(WAIT_FOR_TEST_COMPLETION);
-
