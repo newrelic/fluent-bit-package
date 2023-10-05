d1f6c0bd2717bec13e339f3c418525ac813926d7
Lucas <luckslovez@gmail.com>
2023-10-05 12:20:44 +0200

    NR-125500 test-suite linux

--- integration-tests/test-suite/lib/retryingAxios.js
+++ integration-tests/test-suite/lib/retryingAxios.js
@@ -20,7 +20,7 @@ axiosRetry(axios, {
       // If the error indicates that the service didn't process the request successfully, safe to try again
       axiosRetry.isRetryableError(error) ||
       // isRetryableError does not include 429. We're over our rate limit for the moment,
-      // but if we wait a bit, we may no longer be rate limited (Vortex rate limits just for a minute)
+      // but if we wait a bit, we may no longer be rate limited
       error.response.status === 429
       // Note that we don't use `axiosRetry.isIdempotentRequestError`, since that function doesn't
       // think that POST should be retried (since it's not idempotent). However, all the above error
