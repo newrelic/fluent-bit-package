0daf834e7d61e7fb8f1b261cc703bad352e7ba18
Lucas <luckslovez@gmail.com>
2023-10-25 12:27:40 +0200

    Use lib to create(if necessary) the log source in windows

--- integration-tests/test-suite/fluent-bit-tests.test.js
+++ integration-tests/test-suite/fluent-bit-tests.test.js
@@ -7,6 +7,7 @@ const { v4: uuidv4 } = require('uuid');
 const { currentTimeAsIso8601 } = require('./lib/time');
 const { spawnSync } = require('child_process');
 const { requireEnvironmentVariable } = require('./lib/environmentVariables');
+const { EventLog } = require("node-eventlog");
 
 /**
  * The newline is important -- Fluent Bit will wait
@@ -59,13 +60,13 @@ const rfc5424 = (message) => {
 
 const executeSync = (command, commandArguments, expectedExitCode) => {
   const result = spawnSync(command, commandArguments);
-  
-  expect(result.status).toEqual(expectedExitCode);
+
   logger.info(result.stdout?.toString());
   logger.error(result.stderr?.toString());
+  expect(result.status).toEqual(expectedExitCode);
 }
 
-const causeJournaldMessageToBeWrittenForSsh = (uuid) => {  
+const causeJournaldMessageToBeWrittenForSsh = (uuid) => {
   // This _attempts_ to make an SSH connecting using the UUID as a user.
   // It will fail to connect, of course, since the UUID isn't a user,
   // but the attempt will be logged to journald for sshd with the name
@@ -76,44 +77,30 @@ const causeJournaldMessageToBeWrittenForSsh = (uuid) => {
     // (otherwise SSH will not attempt to connect)
     '-t',
     // Don't get prompted whether we trust the host's authenticity
-    // (since we're not a terminal and can't approve it, then SSH 
-    //  will not attempt to connect. And anyway, it's localhost, 
+    // (since we're not a terminal and can't approve it, then SSH
+    //  will not attempt to connect. And anyway, it's localhost,
     //  so we trust it :P)
     '-o',  'StrictHostKeyChecking=false',
     `${uuid}@localhost`];
-  
+
     // It should return 255 because of "Permission denied"
     const expectedExitCode = 255;
-  
+
   executeSync(command, commandArguments, expectedExitCode);
 };
 
-const createWindowsEventLogSource = (logName, source) => {
-  const expectedExitCode = 0;
-  const command = `[System.Diagnostics.EventLog]::CreateEventSource("${source}", "${logName}")`
-  
-  executeSync('powershell', [command], expectedExitCode);
-}
-
-const createWindowsEventLogMessage = (logName, source, message) => {
-  const expectedExitCode = 0;
-  const command = `Write-EventLog -LogName "${logName}" -Source "${source}" -EventID 3001 -EntryType Information -Message "${message}"`
-  
-  executeSync('powershell', [command], expectedExitCode);
-}
-
-const causeEventToBeWrittenToWindowsApplicationLog = (logName, source, message) => {
+const causeEventToBeWrittenToWindowsApplicationLog = async (logName, source, message) => {
   // Create a source so that for debugging we can easily just look
   // at logs created by us (this is just a nice to have, we could also
   // just write as some existing source)
-  createWindowsEventLogSource(logName, source);
-  
-  createWindowsEventLogMessage(logName, source, message);
+
+  const winEvtLogger = new EventLog(source);
+  await winEvtLogger.log(message, 'info', 3001);
 }
 
 /**
  * This tests all things directly configurable from the Infrastructure Agent.
- * 
+ *
  * See https://docs.newrelic.com/docs/logs/forward-logs/forward-your-logs-using-infrastructure-agent.
  */
 describe('Infrastructure Agent Fluent Bit features', () => {
@@ -123,22 +110,22 @@ describe('Infrastructure Agent Fluent Bit features', () => {
     const accountId = requireEnvironmentVariable('ACCOUNT_ID');
     const apiKey = requireEnvironmentVariable('API_KEY');
     const nerdGraphUrl = requireEnvironmentVariable('NERD_GRAPH_URL');
-      
+
     // Read configuration
     nrdb = newNrdb({ accountId, apiKey, nerdGraphUrl });
-    
+
   });
 
   const waitForLogMessageContaining = async (substring) => {
     return nrdb.waitToFindOne({ where: `message like '%${substring}%'` });
   }
-  
+
   const testOnlyIfSet = (environmentVariableName) => {
     return process.env[environmentVariableName] ? test : test.skip;
   }
 
   testOnlyIfSet('MONITORED_FILE')('detects appending to a file', async () => {
-    // Create a string with a unique value in it so that we can find it later 
+    // Create a string with a unique value in it so that we can find it later
     const uuid = uuidv4();
     const line = `fluent-bit-tests: tail ${uuid}`;
 
@@ -151,7 +138,7 @@ describe('Infrastructure Agent Fluent Bit features', () => {
   });
 
   testOnlyIfSet('MONITORED_TCP_PORT')('detects writing to TCP port', async () => {
-    // Create a string with a unique value in it so that we can find it later 
+    // Create a string with a unique value in it so that we can find it later
     const uuid = uuidv4();
     const line = `fluent-bit-tests: tcp ${uuid}`;
 
@@ -164,7 +151,7 @@ describe('Infrastructure Agent Fluent Bit features', () => {
   });
 
   testOnlyIfSet('MONITORED_SYSLOG_RFC_5424_TCP_PORT')('detects writing to a TCP socket with a syslog RFC 5424 message', async () => {
-    // Create a string with a unique value in it so that we can find it later 
+    // Create a string with a unique value in it so that we can find it later
     const uuid = uuidv4();
     const message = `fluent-bit-tests: syslog (TCP socket - RFC 5424) ${uuid}`;
     const syslog = rfc5424(message);
@@ -178,7 +165,7 @@ describe('Infrastructure Agent Fluent Bit features', () => {
   });
 
   testOnlyIfSet('MONITORED_SYSLOG_RFC_5424_UDP_PORT')('detects writing to a UDP socket with a syslog RFC 5424 message', async () => {
-    // Create a string with a unique value in it so that we can find it later 
+    // Create a string with a unique value in it so that we can find it later
     const uuid = uuidv4();
     const message = `fluent-bit-tests: syslog (UDP socket - RFC 5424) ${uuid}`;
     const syslog = rfc5424(message);
@@ -195,7 +182,7 @@ describe('Infrastructure Agent Fluent Bit features', () => {
     // This test currently only knows how to create ssh systemd logs
     const monitoredSystemdUnit = requireEnvironmentVariable('MONITORED_SYSTEMD_UNIT');
     expect(monitoredSystemdUnit).toMatch(/^ssh|sshd$/); // This is 'ssh' in some distros, 'sshd' in others
-    
+
     // Create a unique string so that we can find the log message later
     const uuid = uuidv4();
 
@@ -215,15 +202,15 @@ describe('Infrastructure Agent Fluent Bit features', () => {
     const monitoredLogName = requireEnvironmentVariable('MONITORED_WINDOWS_LOG_NAME_USING_WINLOG');
     const source = 'Fluent Bit Tests: winlog';
     const message = `fluent-bit-tests: winlog ${uuid}`;
-    causeEventToBeWrittenToWindowsApplicationLog(monitoredLogName, source, message);
+    await causeEventToBeWrittenToWindowsApplicationLog(monitoredLogName, source, message);
 
     // Wait for that log line to show up in NRDB
-    // 
+    //
     // NOTE: this may take a while, since unlike winevtlog (which just reads
     // new events by default), winlog will read all events in the monitored log
     await waitForLogMessageContaining(message);
   });
-  
+
   testOnlyIfSet('MONITORED_WINDOWS_LOG_NAME_USING_WINEVTLOG')('detects a Windows event using "winevtlog" input plugin', async () => {
     // Create a unique string so that we can find the log message later
     const uuid = uuidv4();
@@ -232,7 +219,7 @@ describe('Infrastructure Agent Fluent Bit features', () => {
     const monitoredLogName = requireEnvironmentVariable('MONITORED_WINDOWS_LOG_NAME_USING_WINEVTLOG');
     const source = 'Fluent Bit Tests: winevtlog';
     const message = `fluent-bit-tests: winevtlog ${uuid}`;
-    causeEventToBeWrittenToWindowsApplicationLog(monitoredLogName, source, message);
+    await causeEventToBeWrittenToWindowsApplicationLog(monitoredLogName, source, message);
 
     // Wait for that log line to show up in NRDB
     await waitForLogMessageContaining(message);
