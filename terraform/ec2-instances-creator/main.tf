ace138869d9dea19d6e656e756bf13f85ba9395b
Lucas <luckslovez@gmail.com>
2023-12-18 18:13:22 +0100

    original name

--- terraform/ec2-instances-creator/main.tf
+++ terraform/ec2-instances-creator/main.tf
@@ -23,7 +23,7 @@ locals {
   # Debian AMIs do not come with the SSM Agent installed by default: https://docs.aws.amazon.com/systems-manager/latest/userguide/agent-install-deb.html
   # The user data script referenced below takes care of installing the SSM Agent and starting it on boot for the aforementioned OSes.
   os_distros_requiring_user_data_script_for_ssm = ["sles", "centos", "debian"]
-  user_data_script_for_ssm_path = "${path.module}/user_data_script.tftpl"
+  user_data_script_for_ssm_path = "${path.module}/user_data_script_for_ssm.tftpl"
 
   # Default tags applied to all created resources
   default_tags = {
