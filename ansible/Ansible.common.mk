d8e53e3f1859839a567b46dc0f149146a676758f
dsharma <dsharma@newrelic.com>
2024-05-08 16:52:00 +0530

    setting ansible script path

--- ansible/Ansible.common.mk
+++ ansible/Ansible.common.mk
@@ -18,11 +18,17 @@ $(ROLES_PATH) $(COLLECTIONS_PATH):
 	@mkdir -p $@
 
 # Installs dependencies into "collections" and "roles" folders
+.PHONY: run_ansible_role_key
+run_ansible_role_key: ANSIBLE_FOLDER=$(ANSIBLE_FOLDER) ANSIBLE_ENV_SCRIPT=$(ANSIBLE_FOLDER)/set_ansible_env.sh
+    
 .PHONY: dependencies
 dependencies: $(ROLES_PATH) $(COLLECTIONS_PATH)
+ifdef ANSIBLE_ENV_SCRIPT
+	sh $(ANSIBLE_ENV_SCRIPT)
+else
 	ansible-galaxy role install -r $(REQUIREMENTS_FILE) -p $(ROLES_PATH)
 	ansible-galaxy collection install -r $(REQUIREMENTS_FILE) -p $(COLLECTIONS_PATH)
-
+endif
 .PHONY: prepare-inventory
 prepare-inventory:
 	@sed "s/PRE_RELEASE_NAME/${PRE_RELEASE_NAME}/g" $(ANSIBLE_INVENTORY_TEMPLATE) > $(ANSIBLE_INVENTORY)
