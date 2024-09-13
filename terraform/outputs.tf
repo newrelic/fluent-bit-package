0c96f3d3e874dd9937f83649a921cb7a22c8d86f
Himanshu Rai <hrai@newrelic.com>
2024-09-13 16:55:47 +0530

    adding everything

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,3 +1,4 @@
 output "instance_ids" {
-  value = aws_instance.my_instances[*].id
+  description = "A map of instance IDs created by the ec2_instance module"
+  value       = { for k, v in module.ec2_instance : k => v.id }
 }
