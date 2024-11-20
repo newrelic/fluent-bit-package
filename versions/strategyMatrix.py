8829e979e40299d8aa6576346684846db9d1324e
HEMAGUNNALA <hgunnala@newrelic.com>
2024-11-20 10:15:23 +0530

    add sign_suse_packages back and test amazonlinux 2023

--- versions/strategyMatrix.py
+++ versions/strategyMatrix.py
@@ -194,7 +194,7 @@ def list_distro_files():
         return [
             filename
             for filename in os.listdir(".")             
-            if (filename.startswith("sles_15.3") or filename.startswith("amazonlinux_2023"))
+            if (filename.startswith("amazonlinux_2023"))
             and filename != "common.yml"
         ]
     except Exception as e:
