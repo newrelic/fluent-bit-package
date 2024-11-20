10f4971786f2d6f4de819d189272f0f78c2e957f
HEMAGUNNALA <hgunnala@newrelic.com>
2024-11-20 09:45:53 +0530

    test sles_15.3

--- versions/strategyMatrix.py
+++ versions/strategyMatrix.py
@@ -194,7 +194,7 @@ def list_distro_files():
         return [
             filename
             for filename in os.listdir(".")             
-            if (filename.startswith("amazonlinux_2023"))
+            if (filename.startswith("sles_15.3"))
             and filename != "common.yml"
         ]
     except Exception as e:
