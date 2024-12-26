35e59ed89c38f47202615994397bc38e37f9436e
sramesh <sramesh@newrelic.com>
2024-12-27 05:15:29 +0530

    add sles

--- versions/strategyMatrix.py
+++ versions/strategyMatrix.py
@@ -186,7 +186,7 @@ def list_distro_files():
         return [
             filename
             for filename in os.listdir(".")             
-            if (filename.startswith("amazonlinux_2") or filename.startswith("sles_15.3"))
+            if (filename.startswith("amazonlinux_2") or filename.startswith("sles_12.5"))
             and filename != "common.yml"
         ]
     except Exception as e:
