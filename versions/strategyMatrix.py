a0e3927c8fa73e3c51dd79a8f2a8364cd4608100
sramesh <sramesh@newrelic.com>
2024-12-26 23:28:26 +0530

    ubuntu 22

--- versions/strategyMatrix.py
+++ versions/strategyMatrix.py
@@ -186,7 +186,7 @@ def list_distro_files():
         return [
             filename
             for filename in os.listdir(".")             
-            if (filename.startswith("amazonlinux_2023"))
+            if (filename.startswith("ubuntu_22"))
             and filename != "common.yml"
         ]
     except Exception as e:
