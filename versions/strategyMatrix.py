d6a0f00c6cff75b1c793007f001a65878855f7b1
HEMAGUNNALA <hgunnala@newrelic.com>
2025-01-05 14:28:43 +0530

    test centos 7 and update AMIs

--- versions/strategyMatrix.py
+++ versions/strategyMatrix.py
@@ -186,7 +186,7 @@ def list_distro_files():
         return [
             filename
             for filename in os.listdir(".")             
-            if (filename.startswith("centos"))
+            if (filename.startswith("centos_7"))
             and filename != "common.yml"
         ]
     except Exception as e:
