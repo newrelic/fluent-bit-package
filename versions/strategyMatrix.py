f2c04b12a7fe1ae98db8c1e4798cd478b923233f
HEMAGUNNALA <hgunnala@newrelic.com>
2025-01-06 01:19:37 +0530

    test windows server

--- versions/strategyMatrix.py
+++ versions/strategyMatrix.py
@@ -186,7 +186,7 @@ def list_distro_files():
         return [
             filename
             for filename in os.listdir(".")             
-            if (filename.startswith("centos"))
+            if (filename.startswith("windows-server"))
             and filename != "common.yml"
         ]
     except Exception as e:
