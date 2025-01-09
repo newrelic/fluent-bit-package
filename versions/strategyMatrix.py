f5c417a76e5c6fb262932dc24ea230011e6ddcc0
sramesh <sramesh@newrelic.com>
2025-01-09 22:10:53 +0530

    [testing] immediate failure for fatal errors

--- versions/strategyMatrix.py
+++ versions/strategyMatrix.py
@@ -186,7 +186,7 @@ def list_distro_files():
         return [
             filename
             for filename in os.listdir(".")
-            if (filename.endswith(".yml") or filename.endswith(".yaml")) and not filename.startwith("sles")
+            if (filename.endswith(".yml") or filename.endswith(".yaml"))
             and filename != "common.yml"
         ]
     except Exception as e:
