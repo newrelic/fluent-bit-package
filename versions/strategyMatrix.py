3cd51ea5d9fed6998311845ff13543c1a0a5be08
HEMAGUNNALA <hgunnala@newrelic.com>
2025-01-12 17:20:40 +0530

    test windows

--- versions/strategyMatrix.py
+++ versions/strategyMatrix.py
@@ -186,7 +186,7 @@ def list_distro_files():
         return [
             filename
             for filename in os.listdir(".")             
-            if (filename.startswith("ubuntu"))
+            if (filename.startswith("windows"))
             and filename != "common.yml"
         ]
     except Exception as e:
