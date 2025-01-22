5c97a5ed57170140cf913e8ce0e15864f9b35b92
sramesh <sramesh@newrelic.com>
2025-01-22 19:13:19 +0530

    just ubuntu 24

--- versions/strategyMatrix.py
+++ versions/strategyMatrix.py
@@ -186,7 +186,7 @@ def list_distro_files():
         return [
             filename
             for filename in os.listdir(".")             
-            if (filename.startswith("ubuntu"))
+            if (filename.startswith("ubuntu_24"))
             and filename != "common.yml"
         ]
     except Exception as e:
