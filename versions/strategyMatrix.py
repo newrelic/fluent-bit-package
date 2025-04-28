003ab9a9f975b829a8f7dbafb535e13d8501cab4
sramesh <sramesh@newrelic.com>
2025-04-28 11:15:14 +0530

    windows check

--- versions/strategyMatrix.py
+++ versions/strategyMatrix.py
@@ -186,7 +186,7 @@ def list_distro_files():
         return [
             filename
             for filename in os.listdir(".")             
-            if ((filename.endswith(".yml") or filename.endswith(".yaml")) and filename.startswith("windows_"))
+            if ((filename.endswith(".yml") or filename.endswith(".yaml")) and filename.startswith("windows-"))
             and filename != "common.yml"
         ]
     except Exception as e:
