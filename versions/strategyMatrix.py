d0c58c4cf7664b0ceacc035e063dceccade251f8
sramesh <sramesh@newrelic.com>
2025-04-29 10:23:11 +0530

    revert temp commit

--- versions/strategyMatrix.py
+++ versions/strategyMatrix.py
@@ -186,7 +186,7 @@ def list_distro_files():
         return [
             filename
             for filename in os.listdir(".")             
-            if ((filename.endswith(".yml") or filename.endswith(".yaml")) and filename.startswith("windows-"))
+            if (filename.endswith(".yml") or filename.endswith(".yaml"))
             and filename != "common.yml"
         ]
     except Exception as e:
