6904388037327e378b6e09ae1c664ff7cc82842b
Sanath Ramesh <sanathramesh55@gmail.com>
2025-10-17 15:27:24 +0530

    add debian 13

--- versions/strategyMatrix.py
+++ versions/strategyMatrix.py
@@ -186,7 +186,7 @@ def list_distro_files():
         return [
             filename
             for filename in os.listdir(".")             
-            if (filename.startswith("centos_10"))
+            if (filename.startswith("centos_10") or filename.startswith("debian_13"))
             and filename != "common.yml"
         ]
     except Exception as e:
