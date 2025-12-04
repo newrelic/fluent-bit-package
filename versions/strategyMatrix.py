08f58ebe34b63ef61efc0645ae6a8a5421ff1b74
Sanath Ramesh <sanathramesh55@gmail.com>
2025-12-04 12:45:50 +0530

    revert

--- versions/strategyMatrix.py
+++ versions/strategyMatrix.py
@@ -186,7 +186,7 @@ def list_distro_files():
         return [
             filename
             for filename in os.listdir(".")             
-            if ((filename.endswith(".yml") or filename.endswith(".yaml")) and filename.startswith("debian"))
+            if ((filename.endswith(".yml") or filename.endswith(".yaml")) and not filename.startswith("windows"))
             and filename != "common.yml"
         ]
     except Exception as e:
