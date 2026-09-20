0ec6705e7ca362d2fa36087c56fa5e61a66c75cb
satya-nr <psatyanarayana@newrelic.com>
2026-09-20 10:29:50 +0530

    TEMP: force packages to look missing for a real E2E run

--- versions/strategyMatrix.py
+++ versions/strategyMatrix.py
@@ -80,7 +80,8 @@ WINDOWS_DISTRO = "windows-server"
 
 
 def deb_package_details(pkg):
-    target_package_name = f"fluent-bit_{pkg['fbVersion']}_{pkg['osDistro']}-{pkg['osVersion']}_{pkg['arch']}.deb"
+    # TEMP: makes this package look "new" so CI actually tests it instead of skipping. Revert later.
+    target_package_name = f"fluent-bit_{pkg['fbVersion']}-test-ami-fix_{pkg['osDistro']}-{pkg['osVersion']}_{pkg['arch']}.deb"
     return {
         "packageUrl": f"https://packages.fluentbit.io/{pkg['osDistro']}/{pkg['osVersion']}/fluent-bit_{pkg['fbVersion']}_{pkg['arch']}.deb",
         "targetPackageName": target_package_name,
@@ -97,7 +98,8 @@ def rpm_package_details(pkg):
     rpm_os_family = {"amazonlinux": "amazonlinux", "centos": "el", "rockylinux": "el"}[pkg["osDistro"]]
     pkg_arch = "arm64" if pkg["arch"] == "aarch64" else "x86_64"
     repo_arch = pkg["arch"]
-    target_package_name = f"fluent-bit-{pkg['fbVersion']}-1.{pkg['osDistro']}-{pkg['osVersion']}.{pkg_arch}.rpm"
+    # TEMP: makes this package look "new" so CI actually tests it instead of skipping. Revert later.
+    target_package_name = f"fluent-bit-{pkg['fbVersion']}-test-ami-fix-1.{pkg['osDistro']}-{pkg['osVersion']}.{pkg_arch}.rpm"
     package_manager_type = "yum"
     upload_dest = (
         "{dest_prefix}linux/"
@@ -136,7 +138,8 @@ def sles_package_details(pkg):
 
 def windows_package_details(data):
     windows_target_arch = {"win32": "386", "win64": "amd64"}[data["arch"]]
-    target_package_name = f"fb-windows-{data['fbVersion']}-{windows_target_arch}.zip"
+    # TEMP: makes this package look "new" so CI actually tests it instead of skipping. Revert later.
+    target_package_name = f"fb-windows-{data['fbVersion']}-test-ami-fix-{windows_target_arch}.zip"
     return {
         "packageUrl": f"http://packages.fluentbit.io/windows/fluent-bit-{data['fbVersion']}-{data['arch']}.zip",
         "targetPackageName": target_package_name,
